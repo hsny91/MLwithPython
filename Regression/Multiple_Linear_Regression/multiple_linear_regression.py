11e0b3ba0363b9ddf66df2557a26400bc438f70d
hsny91 <husniyesakmak.91@gmail.com>
2021-11-24 15:09:24 +0100

    regression file was updated

---
+++ Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+df = pd.read_csv("/Users/hsekeroglu/Desktop/Huesniye/MLwithPython/Regression/Multiple_Linear_Regression",sep = ";")
+# linear regression model
+
+
+y = df.maas.values.reshape(-1,1)
+x = df.iloc[:,[0,2]].values
+multiple_linear_regression = LinearRegression()
+multiple_linear_regression.fit(x,y)
+print("bo_", multiple_linear_regression.intercept_)
+print("b1,b2", multiple_linear_regression.coef_)
+multiple_linear_regression.predict(np.array([[10,35],[5,35]]))
