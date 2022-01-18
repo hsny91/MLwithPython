5ce03851cfc0a26129919adeaf5f0288fa360769
hsny91 <husniyesakmak.91@gmail.com>
2022-01-18 22:03:16 +0100

    multi-linear.py was added

--- Regression/Examples/multi_Linear-regression/multi-linear-regression-2.py
+++ Regression/Examples/multi_Linear-regression/multi-linear-regression-2.py
@@ -1,13 +1,8 @@
-import imp
-from re import X
-from statistics import linear_regression
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 df= pd.read_csv("/Users/hsekeroglu/Desktop/Huesniye/MLwithPython/Regression/Examples/multi_Linear-regression/FuelConsumptionCo2.csv")
-
-
 
 from sklearn.linear_model import LinearRegression
 multiLinear_reg= LinearRegression()
