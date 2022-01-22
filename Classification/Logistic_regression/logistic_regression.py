10a5e5a6f42325ac412918f510e892f3562ecf57
Huesniye-Sekeroglu <Huesniye.Sekeroglu@ibm.com>
2022-01-22 19:59:19 +0100

    logistic_regression was created

--- Classification/Logistic_regression/logistic_regression.py
+++ Classification/Logistic_regression/logistic_regression.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv("/Users/hsekeroglu/Desktop/Huesniye/MLwithPython/Classification/Logistic_regression/data.csv")
 
 #id ve unmaned: 32 delete from table
 data.drop(["Unnamed: 32", "id"],axis=1,inplace=True)
@@ -29,3 +29,94 @@ y_train = y_train.T
 y_test = y_test.T
 
 print("x_train shape: ",x_train.shape)
+
+# %% parameter initialize and sigmoid function
+# dimension = 30
+def initialize_weights_and_bias(dimension):
+    
+    w = np.full((dimension,1),0.01)
+    b = 0.0
+    return w,b
+# w,b = initialize_weights_and_bias(30)
+# z = (w.T)x + b =
+#z = np.dot(w.T,x_train)+b
+
+def sigmoid(z): #türev
+    y_head = 1/(1+ np.exp(-z))
+    return y_head
+# print(sigmoid(0))
+# %%
+def forward_backward_propagation(w,b,x_train,y_train):
+    # forward propagation
+    z = np.dot(w.T,x_train) + b
+    y_head = sigmoid(z)
+    loss = -y_train*np.log(y_head)-(1-y_train)*np.log(1-y_head) # -(1-y)log(1-y_head)-ylog(y_head)
+    cost = (np.sum(loss))/x_train.shape[1]      # x_train.shape[1]  is for scaling
+    
+    # backward propagation
+    derivative_weight = (np.dot(x_train,((y_head-y_train).T)))/x_train.shape[1] # x_train.shape[1]  is for scaling
+    derivative_bias = np.sum(y_head-y_train)/x_train.shape[1]                 # x_train.shape[1]  is for scaling
+    gradients = {"derivative_weight": derivative_weight, "derivative_bias": derivative_bias}
+    
+    return cost,gradients
+
+#%% Updating(learning) parameters
+def update(w, b, x_train, y_train, learning_rate,number_of_iterarion):
+    cost_list = []
+    cost_list2 = []
+    index = []
+    
+    # updating(learning) parameters is number_of_iterarion times
+    for i in range(number_of_iterarion):
+        # make forward and backward propagation and find cost and gradients
+        cost,gradients = forward_backward_propagation(w,b,x_train,y_train)
+        cost_list.append(cost)
+        # lets update
+        w = w - learning_rate * gradients["derivative_weight"]
+        b = b - learning_rate * gradients["derivative_bias"]
+        if i % 10 == 0:
+            cost_list2.append(cost)
+            index.append(i)
+            print ("Cost after iteration %i: %f" %(i, cost))
+            
+    # we update(learn) parameters weights and bias
+    parameters = {"weight": w,"bias": b}
+    plt.plot(index,cost_list2)
+    plt.xticks(index,rotation='vertical')
+    plt.xlabel("Number of Iterarion")
+    plt.ylabel("Cost")
+    plt.show()
+    return parameters, gradients, cost_list
+#%%  # prediction
+def predict(w,b,x_test):
+    # x_test is a input for forward propagation
+    z = sigmoid(np.dot(w.T,x_test)+b)
+    Y_prediction = np.zeros((1,x_test.shape[1]))
+    # if z is bigger than 0.5, our prediction is sign one (y_head=1),
+    # if z is smaller than 0.5, our prediction is sign zero (y_head=0),
+    for i in range(z.shape[1]):
+        if z[0,i]<= 0.5:
+            Y_prediction[0,i] = 0
+        else:
+            Y_prediction[0,i] = 1
+
+    return Y_prediction
+def logistic_regression(x_train, y_train, x_test, y_test, learning_rate ,  num_iterations):
+    # initialize
+    dimension =  x_train.shape[0]  # that is 30
+    w,b = initialize_weights_and_bias(dimension)
+    # do not change learning rate
+    parameters, gradients, cost_list = update(w, b, x_train, y_train, learning_rate,num_iterations)
+    
+    y_prediction_test = predict(parameters["weight"],parameters["bias"],x_test)
+
+    # Print test Errors
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(y_prediction_test - y_test)) * 100))
+    
+logistic_regression(x_train, y_train, x_test, y_test,learning_rate = 1, num_iterations = 300)    
+
+#%% sklearn with LR
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(x_train.T,y_train.T)
+print("test accuracy {}".format(lr.score(x_test.T,y_test.T)))
